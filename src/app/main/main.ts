a8b68bfa4c4b993927156433ab46cfd48532e6c3
Default <iraklibilashvili99@gmail.com>
2026-03-30 00:34:42 +0400

    xsad1ds

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -49,5 +49,79 @@ export class Main {
   "id": 10,
   "image" : "/images/10.webp",
   },
+  {
+    "id": 11,
+    "image": "/images/11.webp",
+  },
+  {
+    "id": 12,
+    "image": "/images/12.webp",
+  },
+  {
+    "id": 13,
+    "image": "/images/13.webp",
+  },
+  {
+    "id": 14,
+    "image": "/images/14.webp",
+  },
+  {
+    "id": 15,
+    "image": "/images/15.jpg",
+  },
+  {
+    "id": 16,
+    "image": "/images/16.webp",
+  },
+  {
+    "id": 17,
+    "image": "/images/17.jpg",
+  },
+  {
+    "id": 18,
+    "image": "/images/18.jpg",
+  },
+  {
+    "id": 19,
+    "image": "/images/19.webp",
+  },
+  {
+    "id": 20,
+    "image": "/images/20.webp",
+  },
   ]
+
+
+
+
+  showWork = "Work"
+   workClass = "hideCard"
+   onClickWork() { if(this.showWork == "Work") {
+    this.showWork = "Work."
+  this.workClass = "showCard"
+   } else {
+    this.showWork = "Work"
+    this.workClass = "hideCard"
+   }
+   }
+
+
+
+    navAboutMe = "About Me"
+     showAboutMe = "hideAboutMeContainer"
+
+     onClickAboutMe() {
+      if(this.navAboutMe == "About Me") {
+        this.navAboutMe = "About Me."
+        this.showAboutMe = "showAboutMeContainer"
+      }
+      else {
+        this.navAboutMe = "About Me"
+        this.showAboutMe = "hideAboutMeContainer"
+      }
+     }
+
+     navContact = "Contact"
+     showContact = "hideContact"
+
 }
